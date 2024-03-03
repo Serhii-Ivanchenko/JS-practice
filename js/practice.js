b976d3e2bb87c60fe38b071ae11907b06fb66de9
Serhii-Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-03-03 23:03:01 +0200

    Update practice.js

--- js/practice.js
+++ js/practice.js
@@ -1,4 +1,4 @@
-// ! ПРАКТИКА №1 =========================================================================
+// ! ПРАКТИКА №1 ======================================================================================================================================
 
 //* Розгалуження: if, if...else, else...if
 //TODO: 1 ===================================
@@ -588,7 +588,7 @@
 // }, 0);
 // console.log(totalAveragePlaytimePerGame);
 
-// ! ПРАКТИКА №2 =========================================================================
+// ! ПРАКТИКА №2 ======================================================================================================================================
 // ! Масиви і функції
 //TODO:=========task-01=================
 //Перетворити рядок, що містить слова, розділені комами, в масив слів і вивести кожне слово в окремому рядку.
@@ -919,8 +919,6 @@
 
 // console.log(uniq(arr1, arr2));
 
-
-
 // TODO: Convert number into descending order ================================
 // function descendingOrder(n) {
 //   const digitsArray = n
@@ -944,3 +942,309 @@
 // }
 // console.log(descendingOrder(972273402)); //977432220
 // console.log(descendingOrder(145263));
+
+// ! ПРАКТИКА №3 =====================================================================================================================================
+//! Callback
+//TODO:============task-01=========================
+// Напишіть функцію, яка приймає два числа і колбек-функцію. Функція повинна помножити числа між собою та передати результат дії до колбек-функції. Колбек-функція повинна, у разі, якщо, це число є парним - помножити це число на себе, якщо ні - взяти квадратний корінь з цього числа, та вивести результат у консоль лог.
+
+// const performOperation = (a, b, callback) => callback(a * b);
+
+// const callbackFunction = num => num % 2 === 0 ? Math.pow(num, 2) : Math.sqrt(num);
+
+// const result = performOperation(23, 41, callbackFunction);
+
+// console.log(result);
+
+//TODO:============task-02=========================
+// Напишіть функцію, яка приймає масив і колбек-функцію. Функція повинна повернути перший елемент масиву,
+//який задовольняє умову, визначену в колбек - функції. Якщо такого елементу немає, функція
+// повинна повернути null. Також необхідно написати 2 різні колбеки. При використанні першого колбеку має
+// повертатись перше парне число. При використанні другого - має повертатись перше слово у масиві, що починається з літери
+// "O" у будь - якому реєстрі.
+
+// const numbers = [1, 2, 5, 7, 9, 20];
+// const words = ['apple', 'banana', 'orange', 'pear'];
+
+// const firstElementReturn = (arr, callback) => {
+//   let result = null;
+//   arr.forEach(element => {
+//     if (callback(element) && result === null) {
+//       result = element;
+//     }
+//   });
+//   return result;
+// };
+
+// const isEvenNumber = num => num % 2 === 0;
+
+// const startsWithO = str => str.toLowerCase().startsWith('o');
+
+// console.log(firstElementReturn(numbers, isEvenNumber));
+// console.log(firstElementReturn(words, startsWithO));
+
+//! Array methods
+//TODO:=========task-01=================
+// Дано масив чисел [1, 2, 3, 4, 5]. Створіть новий масив, що містить квадрати кожного елементу вхідного масиву. Очікуваний результат: [1, 4, 9, 16, 25].
+
+// const arr = [1, 2, 3, 4, 5];
+
+// const newArr = arr.map(number => Math.pow(number, 2));
+// console.log(newArr);
+
+//TODO:=========task-02=================
+// Дано масив об'єктів {id: 1, values: [1, 2, 3]}, {id: 2, values: [4, 5, 6]}, {id: 3, values: [7, 8, 9]}. Створіть новий масив, що містить всі значення з масивів values кожного об'єкту, збережених в одному масиві. Очікуваний результат: [1, 2, 3, 4, 5, 6, 7, 8, 9].
+
+// const data = [
+//   { id: 1, values: [1, 2, 3] },
+//   { id: 2, values: [4, 5, 6] },
+//   { id: 3, values: [7, 8, 9] },
+// ];
+
+// const valuesArr = data.flatMap(item => item.values);
+// console.log(valuesArr);
+
+//TODO:=========task-03=================
+// Дано масив об'єктів {name: "John", age: 27}, {name: "Jane", age: 31}, {name: "Bob", age: 19}. Перевірте, чи є хоча б один об'єкт з віком менше 20 років. Очікуваний результат: true.
+
+// const people = [
+//   { name: 'John', age: 27 },
+//   { name: 'Jane', age: 31 },
+//   { name: 'Bob', age: 19 },
+// ];
+// const isAdult = arr => {
+//     return arr.some(person => person.age < 20);
+// }
+// console.log(isAdult(people));
+
+//TODO:=========task-04=================
+//  Дано масив чисел [2, 4, 6, 8, 10]. Перевірте, чи є кожен елемент масиву парним. Очікуваний результат: true.
+
+// const numbers = [2, 4, 6, 8, 10];
+
+// const isEven = arr => {
+//     return arr.every(number => number % 2 === 0)
+// }
+
+// console.log(isEven(numbers));
+
+//TODO:=========task-05=================
+// Дано масив чисел [1, 2, 3, 4, 5]. Знайдіть перший елемент, більший за 3. Очікуваний результат: 4.
+
+// const numbers = [1, 2, 3, 4, 5];
+
+// const numberSearch = arr => {
+//     return arr.find(number => number > 3)
+// }
+
+// console.log(numberSearch(numbers));
+
+//TODO:=========task-06=================
+// Відсортуйте масив чисел [4, 2, 5, 1, 3] у порядку зростання. Очікуваний результат: [1, 2, 3, 4, 5].
+
+// const numbersArray = [4, 2, 5, 1, 3];
+
+// const numbersSort = arr => {
+//     return arr.toSorted((a, b) => a - b)
+// }
+// console.log(numbersSorting(numbersArray));
+
+//TODO:===============task-07===============================
+// Відсортуйте масив рядків ["banana", "orange", "apple", "pear"] у порядку алфавіту. Очікуваний результат: ["apple", "banana", "orange", "pear"].
+
+// const stringArray = ['banana', 'orange', 'apple', 'pear'];
+
+// const stringSort = arr => {
+//   return arr.toSorted((a, b) => a.localeCompare(b));
+// };
+// console.log(stringSort(stringArray));
+
+//TODO:===============task-8===============================
+//  Відсортуйте масив об'єктів {name: "John", age: 27}, {name: "Jane", age: 31}, {name: "Bob", age: 19} за віком у порядку зростання. Очікуваний результат: [{name: "Bob", age: 19}, {name: "John", age: 27}, {name: "Jane", age: 31}].
+
+// const users = [
+//   { name: 'John', age: 27 },
+//   { name: 'Jane', age: 31 },
+//   { name: 'Bob', age: 19 },
+// ];
+
+// const objSort = obj => {
+//   return obj.toSorted((a, b) => a.age - b.age);
+// };
+
+// console.log(objSort(users));
+
+//TODO:=========task-09=================
+// Дано масив об'єктів {name: "John", age: 27}, {name: "Jane", age: 31}, {name: "Bob", age: 19}. Створіть новий масив, що містить тільки об'єкти, в яких вік більше 20 років. Очікуваний результат: [{name: "John", age: 27}, {name: "Jane", age: 31}]
+
+// const user = [
+//   { name: 'John', age: 27 },
+//   { name: 'Jane', age: 31 },
+//   { name: 'Bob', age: 19 },
+// ];
+
+// const adultFilter = obj => {
+//     return obj.filter(user => user.age > 20)
+// }
+
+// console.log(adultFilter(user));
+
+//TODO:=========task-10=================
+// Дано масив чисел [1, 2, 3, 4, 5]. Застосуйте метод reduce для обчислення суми елементів масиву
+
+// const numbers = [1, 2, 3, 4, 5];
+
+// const sumCalc = arr => {
+//   return arr.reduce((acc, item) => acc + item, 0);
+// };
+
+// console.log(sumCalc(numbers));
+
+//TODO:=========task-11=================
+// У вас є масив чисел, і вам потрібно знайти середнє значення цих чисел. Напишіть функцію, яка приймає масив чисел і повертає середнє значення.
+
+// const arr = [1, 2, 3, 4, 5];
+
+// const averageValue = arr => {
+//     return arr.reduce((acc, item) => acc + item / arr.length, 0)
+// }
+// console.log(averageValue(arr));
+
+// ! this
+//TODO:=========task-01=================
+// Створити об'єкт, який представляє користувача. У об'єкті повинні бути наступні поля: ім'я, прізвище, вік, email та метод, який виводить повну інформацію про користувача.
+// const user = {
+//   name: 'Ted',
+//   surname: 'Lasso',
+//   age: 30,
+//   email: 'ted@mail.com',
+
+//   getInfo() {
+//     console.log(this);
+//   },
+
+//   modify(key, value) {
+//     if (key !== 'name' && key !== 'lastName') {
+//       return alert('invalide key');
+//     }
+//     if (value[0] === value[0].toUpperCase() && value.length >= 3) {
+//       return (this[key] = value);
+//     }
+//   },
+// };
+// console.log(user.getInfo());
+// user.modify('name', 'Vlad');
+// console.log(user);
+
+//TODO:=========task-02=================
+// Для обьекта "user", написати метод для зміни ім'я або прізвища(змінити можна лише якесь з цих полів, змінювати або додавати нові потрібно заборонити) з перевіркою на валідність даних(Перша літера має бути у верхньому реєстрі, довжина слова не менше 3 літер)
+
+// user.modify('name', 'Vlad');
+
+//TODO:=========task-03=================
+// Створити глобальну функцію для обьекта "user", що може додавати властивості об'єкту, в контексті якого вона буде викликана. Додати цю функцію як метод об'єкта user, та додати за допомогою неї властивість friends із значенням:
+
+// function addProperty(key, value) {
+//   this[key] = value;
+// }
+// user.addProperty = addProperty;
+
+// user.addProperty('friends', [
+//   {
+//     firstName: 'Mary',
+//     lastName: 'Smith',
+//     age: 32,
+//     email: 'marysmith@hotmail.com',
+//   },
+//   {
+//     firstName: 'Alex',
+//     lastName: 'Johnson',
+//     age: 45,
+//     email: 'alex.johnson@yahoo.com',
+//   },
+//   {
+//     firstName: 'Emily',
+//     lastName: 'Davis',
+//     age: 19,
+//     email: 'emilydavis@gmail.com',
+//   },
+// ]);
+
+// console.log(user);
+
+//TODO:=========task-04=================
+//  Викликати метод user.info() таким чином, щоб він вивів результатом  ({name: 'Bob', lactName: 'Lasso',age: 50, email: 'BodLasso@gmail.com'})
+
+// const obj = {
+//   name: 'Bob',
+//   lastName: 'Lasso',
+//   age: 50,
+//   email: 'BodLasso@gmail.com',
+// };
+
+// user.getInfo.call(obj);
+
+
+//TODO:=========task-05=================
+//  Викличте функцію showFullName у контексті об'єкта user
+
+// function showFullName() {
+//     console.log(`${this.name} ${this.surname}`);
+// }
+
+// showFullName.call(user)
+
+//TODO:=========task-06=================
+// Виправте помилки, щоб код працював
+
+// const product = {
+//   price: 5000,
+
+//   showPrice() {
+//     console.log(this.price);
+//   },
+// };
+
+// function callAction(callback) {
+//   callback();
+// }
+
+// callAction(product.showPrice.bind(product));
+
+//! class
+//TODO:=================task-01=============================
+// Завдання полягає у створенні програми, що дозволяє керувати замовленнями в ресторані.
+
+// #### Для цього потрібно реалізувати клас `Order`, який містить такі приватні властивості:
+// - `tableNumber` - номер столика, де зроблено замовлення
+// - `items` - масив предметів замовлення, де кожен предмет містить ім'я (name) та ціну (price)
+// - `isPaid` - позначає, чи оплачене замовлення (default = false)
+
+// ##### Для класу Order потрібно реалізувати такі методи:
+// - `calculateTotal`() - повертає загальну суму замовлення.
+// - `markAsPaid`() - позначає замовлення як оплачене
+// - `addItem`() - який додає предмет замовлення до масиву "items"
+// - `removeItem`() - який видаляє предмет замовленя з масиву "items"
+// ##### Додатково можна створити гетери та сетери для отримання номера столика, статусу замовлення та самого замовлення
+
+// #### Також потрібно створити клас `MenuItem`, який містить властивості `name` та `price` для предметів замовлення.
+
+// Create menu items
+// const item1 = new MenuItem('Burger', 10);
+// const item2 = new MenuItem('Fries', 5);
+// const item3 = new MenuItem('Drink', 2);
+
+// Create an order
+// const order = new Order(1);
+
+// Add items to the order
+// order.addItem(item1);
+// order.addItem(item2);
+// order.addItem(item3);
+
+// Calculate the total and mark the order as paid
+// const total = order.calculateTotal();
+// order.markAsPaid();
+
+// console.log(`Order for table ${order.tableNumber} - Total: $${total}`);
+// console.log(`Is paid: ${order.isPaid}`);
